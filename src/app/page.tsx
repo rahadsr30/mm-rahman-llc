795dd97b2729a47c21f32d9fe6797dd7655dff57
rahadsr30 <rahadahmedsr30@gmail.com>
2025-01-26 16:55:53 +0600

    working on navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <h2 className="font-public text-title font-semibold">Heading</h2>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,55 @@
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        ...fontFamily,
+        public: ["var(--font-public)"],
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addBase, addUtilities, addComponents }) => {
+      addBase({});
+      addUtilities({});
+      addComponents({
+        ".text-title": {
+          "@apply text-[80px] leading-[96px] font-semibold": {},
+        },
+        ".text-hero": {
+          "@apply text-[40px] leading-[150%] font-normal": {},
+        },
+        ".text-body-title": {
+          "@apply text-[18px] leading-[150%] font-semibold": {},
+        },
+        ".text-body-lg": {
+          "@apply text-[18px] leading-[150%] font-normal": {},
+        },
+        ".text-body-sm": {
+          "@apply text-[16px] leading-[150%] font-medium": {},
+        },
+        ".container": {
+          "@apply max-w-[1220px] w-full mx-auto px-4 sm:px-6 lg:px-8": {},
+        },
+        ".py-section": {
+          "@apply py-20 md:py-28 lg:py-32": {},
+        },
+        ".pt-section": {
+          "@apply pt-20 md:pt-28 lg:pt-32": {},
+        },
+      });
+    }),
+  ],
+} satisfies Config;
